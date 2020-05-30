8eee1cad3e227402dcab8c9a026e5da2f478bae2
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-05-30 14:24:41 -0600

    built layout grid and header navbar components

--- ecom/src/components/layout.js
+++ ecom/src/components/layout.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import Header from "./headernavbar/header";
+import Navbar from "./headernavbar/navbar";
 
 export default class Layout extends Component {
 	render() {
 		return (
-			<div>
-				<h1>ecom</h1>
+			<div className="layout">
 				{this.props.children}
+				<Header />
+				<Navbar />
 			</div>
 		);
 	}
