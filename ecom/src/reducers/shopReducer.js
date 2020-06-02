746d14850b2814d2cd46dcbb7241d748b62c56b3
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-06-02 12:14:29 -0600

    fetch products actions built

--- ecom/src/reducers/shopReducer.js
+++ ecom/src/reducers/shopReducer.js
@@ -1,20 +1,20 @@
-import // SET_SHOP_CATEGORIES
-"../actions/types";
+import { SET_SHOP_PRODUCTS } from "../actions/types";
+
 
 const INITIAL_STATE = {
 	categories: [],
 	selectedCategoryId: 0,
-	productsSelected: []
+	productsSelected: [],
+	products: []
 };
 
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
-		// case SET_SHOP_CATEGORIES:
-		//     return {
-		//         ...state,
-		//         categories: action.payload
-		//     }
-		default:
-			return state;
-	}
+		case SET_SHOP_PRODUCTS
+		return {
+			...state,
+			products: action.payload
+		}
+	default: return state;
+}
 }
