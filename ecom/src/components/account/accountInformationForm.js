79f3693e2e73755f4619eb2e86d86c97c259f471
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-06-02 10:41:33 -0600

    Built out long gray button component and placed the instance of it on account information form to create the change passowrd button

--- ecom/src/components/account/accountInformationForm.js
+++ ecom/src/components/account/accountInformationForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { reduxForm, Field } from "redux-form";
-import { FormInput, FormButton } from "../formFields";
+import { FormInput, FormButton, LongGrayButton } from "../formFields";
+
 import history from "../../history";
 class AccountInformationForm extends Component {
 	render() {
@@ -59,12 +60,15 @@ class AccountInformationForm extends Component {
 					name="zipcode"
 					component={FormInput}
 				/>
-				{/* <Field className='account-information-form__login'
-                onClick={() => history.push('/account')}
-                type='submit'
-                title='Login'
-                name='login'
-                component={FormButton}/> */}
+				<Field
+					className="account-information-form__change-password"
+					onClick={() => console.log("tryna show passwords")}
+					type="button"
+					labelTitle="Password"
+					title="Change Password"
+					name="change-password"
+					component={LongGrayButton}
+				/>
 			</form>
 		);
 	}
