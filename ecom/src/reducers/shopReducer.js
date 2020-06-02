2ce7a89c06d459f67dc6a400e47fae1d38506e25
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-06-02 12:20:53 -0600

    Added a missing colon to SET_SHOP_PRODUCTS in shopReducer

--- ecom/src/reducers/shopReducer.js
+++ ecom/src/reducers/shopReducer.js
@@ -1,6 +1,5 @@
 import { SET_SHOP_PRODUCTS } from "../actions/types";
 
-
 const INITIAL_STATE = {
 	categories: [],
 	selectedCategoryId: 0,
@@ -10,11 +9,12 @@ const INITIAL_STATE = {
 
 export default function (state = INITIAL_STATE, action) {
 	switch (action.type) {
-		case SET_SHOP_PRODUCTS
-		return {
-			...state,
-			products: action.payload
-		}
-	default: return state;
-}
+		case SET_SHOP_PRODUCTS:
+			return {
+				...state,
+				products: action.payload
+			};
+		default:
+			return state;
+	}
 }
