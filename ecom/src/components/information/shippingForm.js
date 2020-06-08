2807308a6419ec4ebdaf7298eba3d8e9de5425d7
Cameron Crepeaux <cameroncrapo@overstock.com>
2020-06-08 11:26:15 -0600

    built order summary component, infotitle and underlined components

--- ecom/src/components/information/shippingForm.js
+++ ecom/src/components/information/shippingForm.js
@@ -6,6 +6,8 @@ import { FormInput, FormButton } from "../formFields";
 
 import history from "../../history";
 
+import OrderSummary from "./orderSummary";
+
 class ShippingForm extends Component {
 	render() {
 		const { className, handleSubmit } = this.props;
@@ -13,64 +15,66 @@ class ShippingForm extends Component {
 		return (
 			<form onSubmit={handleSubmit} className={`${className} shipping-form`}>
 				<Field
-					className="shipping-form__name"
-					type="name"
-					title="Name"
-					placeholder="Name"
-					name="name"
+					className='shipping-form__name'
+					type='name'
+					title='Name'
+					placeholder='Name'
+					name='name'
 					component={FormInput}
 				/>
 				<Field
-					className="shipping-form__address"
-					type="address"
-					title="Street Address"
-					placeholder="Street Address"
-					name="address"
+					className='shipping-form__address'
+					type='address'
+					title='Street Address'
+					placeholder='Street Address'
+					name='address'
 					component={FormInput}
 				/>
 				<Field
-					className="shipping-form__city"
-					type="city"
-					title="City"
-					placeholder="City"
-					name="city"
+					className='shipping-form__city'
+					type='city'
+					title='City'
+					placeholder='City'
+					name='city'
 					component={FormInput}
 				/>
 				<Field
-					className="shipping-form__state"
-					type="state"
-					title="State"
-					placeholder="State"
-					name="state"
+					className='shipping-form__state'
+					type='state'
+					title='State'
+					placeholder='State'
+					name='state'
 					component={FormInput}
 				/>
 				<Field
-					className="shipping-form__zipcode"
-					type="zipcode"
-					title="Zipcode"
-					placeholder="Zipcode"
-					name="zipcode"
+					className='shipping-form__zipcode'
+					type='zipcode'
+					title='Zipcode'
+					placeholder='Zipcode'
+					name='zipcode'
 					component={FormInput}
 				/>
 
-				<div className="shipping-form__line"></div>
+				<div className='shipping-form__line'></div>
 				<Field
-					className="shipping-form__use-this-address"
+					className='shipping-form__use-this-address'
 					onClick={() => history.push("/information/payment")}
-					type="submit"
-					title="Use This Address"
-					name="use-this-address"
+					type='submit'
+					title='Use This Address'
+					name='use-this-address'
 					component={FormButton}
 				/>
 				<Field
-					className="shipping-form__back"
+					className='shipping-form__back'
 					onClick={() => history.push("/signin")}
-					type="button"
-					title="Back"
-					name="back"
+					type='button'
+					title='Back'
+					name='back'
 					short={true}
 					component={FormButton}
 				/>
+
+				<OrderSummary className='shipping-form__summary' />
 			</form>
 		);
 	}
